f4e98cc207827b19c988e59c83fb73a214962001
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-12-03 11:22:47 -0500

    Strings manipulations

--- app.js
+++ app.js
@@ -19,3 +19,24 @@ pseudocoding: "write the scope of work in plain text as a comment, describe the 
 // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
 let answer =
   "think about the question and how to impliment it for maybe 60% and im pliment it in coding: 40%";
+
+//   B. Strings
+// For all other questions that involve writing code, you can solve them via the following instructions.
+
+// Create a variable called firstVariable
+let firstVariable;
+// Assign it the value of the string "Hello World"
+firstVariable = "Hello World";
+// Change the value of this variable to some number
+firstVariable = "Wazuuuup!!!";
+// Store the value of firstVariablein a new variable called secondVariable
+let secondVariable = firstVariable;
+
+// Change the value of secondVariableto any string.
+secondVariable = "Yooo! Wazuuuuuup!!!";
+// What is the value of firstVariable?
+("Wazuuuup!!!");
+// Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+// ex: Hello, my name is Jean Valjean
+let myName = "Ramzi Karkoub";
+let newName = `Hello, my name is ${myName}`;
