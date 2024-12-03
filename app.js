27a3fedafe072bbc66b791a9c223c80c02b34e62
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-12-03 11:13:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Section 1
+
+// I. Variables & Datatypes
+
+// A. Q + A
+
+// How do we assign a value to a variable?
+let firstName = "Ramzi";
+// How do we change the value of a variable?
+firstName = "karkoub";
+// How do we assign an existing variable to a new variable?
+let newVariable = firstName;
+// Remind me, what are declare, assign, and define?
+Declare: " create avariable using var, let or const";
+Assign: "give a value to a variable";
+Define: "Combining declaration and assignment";
+// What is pseudocoding and why should you do it?
+pseudocoding: "write the scope of work in plain text as a comment, describe the step that will do to inpliment the solution";
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+let answer =
+  "think about the question and how to impliment it for maybe 60% and im pliment it in coding: 40%";
